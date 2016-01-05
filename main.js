2d6c4f322b1a4a5a55cf5aa509fd886c4163dccc
theborakompanioni <theborakompanioni+github@gmail.com>
2016-01-05 20:04:55 +0100

    add icon

--- main.js
+++ main.js
@@ -2,13 +2,16 @@
 const electron = require('electron');
 const app = electron.app;  // Module to control application life.
 const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
+const Tray = electron.Tray;  // Module to create native browser window.
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 var mainWindow;
 
+var mainIconPath = __dirname + '/bK.png';
+
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
@@ -18,21 +21,35 @@ app.on('window-all-closed', function() {
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.on('ready', function() {
+app.on('ready', function () {
+    var appIcon = new Tray(mainIconPath);
+    appIcon.setToolTip(app.getName());
+    appIcon.setTitle(app.getName());
+
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 660});
+    mainWindow = new BrowserWindow({
+        center: true,
+        title: app.getName(),
+        width: 800,
+        height: 660,
+        resizable: false,
+        frame: true,
+        darkTheme: false,
+        autoHideMenuBar: true,
+        icon: mainIconPath
+    });
 
     // and load the index.html of the app.
     mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-    // Disabled Menu Bar
+    // Disabled Menu Bar completely
     mainWindow.setMenu(null);
 
     // Open the DevTools.
     // mainWindow.webContents.openDevTools();
 
     // Emitted when the window is closed.
-    mainWindow.on('closed', function() {
+    mainWindow.on('closed', function () {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
